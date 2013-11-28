161aa5ecd166878ef16dc225d10494d874d9c89a
James Pace <james.a.pace@gmail.com>
2013-11-28 22:57:33 +0000

    Fix the scrolling issue and persist the user to local storage

--- src/main/webapp/WEB-INF/js/message/messageCollectionView.js
+++ src/main/webapp/WEB-INF/js/message/messageCollectionView.js
@@ -2,10 +2,10 @@ Chat.module("Message", function(Message, Chat, Backbone, Marionette, $, _){
 	Message.MessageCollectionView = Marionette.CollectionView.extend({
 		el: "#messageList",
 		itemView: Message.MessageView,
-		onAfterItemAdded: function(viewInstance){
-			this.$el.stop().animate({
-				scrollTop: viewInstance.$el.offset().top
-			});
+		onAfterItemAdded: function(){
+			this.$el.parent("#messageListWrapper").stop().animate({
+				scrollTop: this.$el.height()
+			}, "slow");
 		}
 	});
 });
--- src/main/webapp/WEB-INF/js/user/controller.js
+++ src/main/webapp/WEB-INF/js/user/controller.js
@@ -1,22 +1,31 @@
 Chat.module("User", function(User, Chat, Backbone, Marionette, $, _){
 	User.Controller = Marionette.Controller.extend({
 		initialize: function(){
-			this.userNames = [
-				"Walter White",
-				"Skyler White",
-				"Jesse Pinkman",
-				"Hank Schrader",
-				"Marie Schrader",
-				"Walter White, Jr.",
-				"Saul Goodman",
-				"Gustavo Fring",
-				"Mike Ehrmantraut",
-				"Todd Alquist",
-				"Lydia Rodarte-Quayle"
-			];
-			this.user = new User.User({
-				name: _.sample(this.userNames)
+			if(localStorage.getItem("user")){
+				this.user = new User.User(JSON.parse(localStorage.getItem("user")));
+			}
+			else{
+				this.user = new User.User({
+					name: _.sample([
+						"Walter White",
+						"Skyler White",
+						"Jesse Pinkman",
+						"Hank Schrader",
+						"Marie Schrader",
+						"Walter White, Jr.",
+						"Saul Goodman",
+						"Gustavo Fring",
+						"Mike Ehrmantraut",
+						"Todd Alquist",
+						"Lydia Rodarte-Quayle"
+					])
+				});
+				localStorage.setItem("user", JSON.stringify(this.user));
+			}
+			this.listenTo(this.user, "change", function(){
+				localStorage.setItem("user", JSON.stringify(this.user));
 			});
+
 			new User.View({
 				model: this.user
 			}).render();
